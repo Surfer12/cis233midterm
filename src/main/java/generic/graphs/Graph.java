1cff8fc4e2aee6694148ac7dd1923a6936621dde
Ryan Oates <ryanoatsie@outlook.com>
2024-11-26 00:09:04 -0800

    refactor: update Graph interface to include overloaded addEdge method and address deprecation warnings in build reports

--- src/main/java/generic/graphs/Graph.java
+++ src/main/java/generic/graphs/Graph.java
@@ -6,6 +6,7 @@ import java.util.Set;
 public interface Graph<T> {
     void addVertex(T vertex);
     void addEdge(T source, T destination, int weight); // Updated method signature
+    void addEdge(T source, T destination);
     void removeVertex(T vertex);
     void removeEdge(T source, T destination);
     Set<T> getNeighbors(T vertex);
