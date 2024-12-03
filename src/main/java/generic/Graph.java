de55f7d41a491f65cc0637a780c06162b207efba
Ryan Oates <ryanoatsie@outlook.com>
2024-12-03 10:58:12 -0800

    refactor: enhance Graph interface and GenericDFS class to support weighted edges and improve functionality

--- src/main/java/generic/Graph.java
+++ src/main/java/generic/Graph.java
@@ -1,8 +1,13 @@
 package generic;
 import java.util.Set;
 
-public interface Graph< T> {    
-
+public interface Graph<T> {    
+    void dfs(T node, Set<T> visited, Graph<T> graph);
+    void addVertex(T vertex);
+    void addEdge(T source, T destination);
+    void addEdge(T source, T destination, int weight);
+    void removeVertex(T vertex);
+    void removeEdge(T source, T destination);
     Set<T> getNeighbors(T node);
-
+    void printGraph();
 }
--- src/main/java/generic/dfs/GenericDFS.java
+++ src/main/java/generic/dfs/GenericDFS.java
@@ -1,13 +1,16 @@
-package generic.dfs;        
+package generic.dfs;
+
 import java.util.*;
 import generic.Graph;
 
 public class GenericDFS<T> implements Graph<T> {
     private Map<T, Set<T>> adjacencyList = new HashMap<>();
-    
+    private Map<T, Map<T, Integer>> weightedAdjacencyList = new HashMap<>();
+
     @Override
     public void dfs(T node, Set<T> visited, Graph<T> graph) {
-        if (node == null) return;
+        if (node == null)
+            return;
 
         visited.add(node);
         System.out.print(node + " ");
@@ -23,6 +26,7 @@ public class GenericDFS<T> implements Graph<T> {
     public void addVertex(T vertex) {
         if (!adjacencyList.containsKey(vertex)) {
             adjacencyList.put(vertex, new HashSet<>());
+            weightedAdjacencyList.put(vertex, new HashMap<>());
             System.out.println("Vertex " + vertex + " added");
         }
     }
@@ -36,24 +40,37 @@ public class GenericDFS<T> implements Graph<T> {
 
     @Override
     public void addEdge(T source, T destination, int weight) {
-        // Implementation for adding weighted edges
+        adjacencyList.putIfAbsent(source, new HashSet<>());
+        adjacencyList.putIfAbsent(destination, new HashSet<>());
+        adjacencyList.get(source).add(destination);
+
+        weightedAdjacencyList.putIfAbsent(source, new HashMap<>());
+        weightedAdjacencyList.get(source).put(destination, weight);
     }
 
     @Override
     public void removeVertex(T vertex) {
         adjacencyList.remove(vertex);
+        weightedAdjacencyList.remove(vertex);
+
         for (T neighbor : adjacencyList.keySet()) {
-            adjacencyList.get(neighbor).remove(vertex);   
+            adjacencyList.get(neighbor).remove(vertex);
+            weightedAdjacencyList.get(neighbor).remove(vertex);
+
             if (adjacencyList.get(neighbor).isEmpty()) {
                 adjacencyList.remove(neighbor);
+                weightedAdjacencyList.remove(neighbor);
             }
         }
     }
 
     @Override
-    public void removeEdge(T source, T destination) {        
+    public void removeEdge(T source, T destination) {
         adjacencyList.get(source).remove(destination);
-        adjacencyList.get(destination).remove(source);     
+        adjacencyList.get(destination).remove(source);
+
+        weightedAdjacencyList.get(source).remove(destination);
+        weightedAdjacencyList.get(destination).remove(source);
     }
 
     @Override
@@ -71,8 +88,10 @@ public class GenericDFS<T> implements Graph<T> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof GenericDFS)) return false;
+        if (this == o)
+            return true;
+        if (!(o instanceof GenericDFS))
+            return false;
         GenericDFS<?> that = (GenericDFS<?>) o;
         return Objects.equals(adjacencyList, that.adjacencyList);
     }
